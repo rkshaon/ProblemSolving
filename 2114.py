c89b07c54ef18d6c0737a28d1a1d1e7dab28c00c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-06-18 00:26:12 +0600

    leetcode # 2114 - solve

--- 2114.py
+++ 2114.py
@@ -2,3 +2,13 @@
 # 2114
 # Maximum Number of Words Found in Sentences
 
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        max_count = 0
+        
+        for sentence in sentences:
+            if len(sentence.split(' ')) > max_count:
+                max_count = len(sentence.split(' '))
+        
+        return max_count
+        
