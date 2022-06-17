d6e58fcc637c9a20f49335161b214448a537788b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-06-17 23:19:39 +0600

    leetcode # 13 - tried

---
+++ 13.py
@@ -0,0 +1,20 @@
+# LeetCode
+# 13
+# Roman to Integer
+
+ROMAN_VALUE = {
+    "I": 1,
+    "II": 2,
+    "III": 3,
+    "IV": 4,
+    "V": 5,
+    "VI": 6,
+    "VII": 7,
+    "VIII": 8,
+    "IX": 9,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
