4a28c61154196489817b14f570919a7ddae65eb4
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-06-17 23:47:11 +0600

    leetcode # 1693 - solve

---
+++ 1693.sql
@@ -0,0 +1,7 @@
+-- LeetCode
+-- 1693
+-- Daily Leads and Partners
+
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS 'unique_leads', COUNT(DISTINCT partner_id) AS  'unique_partners'
+FROM DailySales
+GROUP BY date_id, make_name;
