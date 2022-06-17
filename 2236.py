db5c7532eb484e856ca77d13f2690ad841549d20
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-06-18 00:14:33 +0600

    leetcode # 2236 - solve

---
+++ 2236.py
@@ -0,0 +1,17 @@
+# LeetCode
+# 2236
+# Root Equals Sum of Children
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val == (root.left.val + root.right.val):
+            return True
+        
+        return False
+        
