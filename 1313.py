83e9161c4e060ef07fd8296e78456a9631b94a48
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-06-18 15:39:38 +0600

    leetcode # 1313 - solve

--- 1313.py
+++ 1313.py
@@ -3,6 +3,10 @@
 # Decompress Run-Length Encoded List
 # https://leetcode.com/problems/decompress-run-length-encoded-list/
 
+# Solution 1
+# Runtime: 122ms
+# Memory Usage: 14.4MB
+
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         result = []
@@ -13,15 +17,14 @@ class Solution:
         
         return result
 
-# nums = [1,2,3,4]
-
-# def decompressList(nums):
-#     result = []
-
-#     for i in range(0, len(nums), 2):
-#         for j in range(nums[i]):
-#             result.append(nums[i+1])
-
-#     return result
-
-# print(decompressList(nums))
+# Solution 2
+# Runtime: 76ms
+# Memory Usage: 14.3MB
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        result = []
+        
+        for i in range(0, len(nums), 2):
+            result += [nums[i+1] for j in range(nums[i])]
+        
+        return result
