f90a45f7826f37333a4f0fa8f95f4d91e1e45d59
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-02 00:44:47 +0600

    leetcode # 1528 - solved

--- 1528.py
+++ 1528.py
@@ -4,6 +4,12 @@
 # https://leetcode.com/problems/shuffle-string/
 
 def restore_string(s, indices):
-    return ''.join(s[i] for i in indices)
+    temp_result = [None] * len(s)
+
+    for i in range(len(s)):
+        temp_result[indices[i]] = s[i]
+
+    return ''.join(i for i in temp_result)
 
 print(restore_string('codeleet', [4,5,6,7,0,2,1,3]))
+print(restore_string('abc', [0,1,2]))
