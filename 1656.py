25931e4ee7cf5677d3b3d09ffb695dc8d67d9426
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-17 00:21:24 +0600

    leetcode # 2367 - started

---
+++ 1656.py
@@ -0,0 +1,20 @@
+# 1656
+# Design an Ordered Stream
+# Easy
+# https://leetcode.com/problems/design-an-ordered-stream/
+# Array, Hash Table, Design, Data Stream
+
+class OrderedStream:
+
+    def __init__(self, n: int):
+        pass
+        
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        pass
+        
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
---
+++ 2367.py
@@ -0,0 +1,10 @@
+# 2367
+# Number of Arithmetic Triplets
+# Easy
+# https://leetcode.com/problems/number-of-arithmetic-triplets/
+
+def arithmeticTriplets(nums, diff):
+    counts = set()
+    return counts
+
+print(arithmeticTriplets(nums = [0,1,4,6,7,10], diff = 3))
