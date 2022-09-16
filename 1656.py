5648ba6c41c37bb1aa4c409d507b16b0b437027e
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-17 00:27:03 +0600

    leetcode # 1656 - solved

--- 1656.py
+++ 1656.py
@@ -7,11 +7,22 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        pass
+        self.list = [None] * n
+        self.pointer = 0
         
 
-    def insert(self, idKey: int, value: str) -> List[str]:
-        pass
+    # def insert(self, idKey: int, value: str) -> List[str]:
+    #     pass
+    def insert(self, idKey: int, value: str) -> list[str]:
+        self.list[idKey-1] = value
+
+        result = []
+
+        while self.pointer < len(self.list) and self.list[self.pointer] is not None:
+            result.append(self.list[self.pointer])
+            self.pointer += 1
+        
+        return result
         
 
 
