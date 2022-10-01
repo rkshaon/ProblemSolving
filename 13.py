5c72f36a1a15e230a96485610f82b5a9b37ac956
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-10-01 23:29:51 +0600

    leetcode # 13 - tried

--- 13.py
+++ 13.py
@@ -2,19 +2,14 @@
 # 13
 # Roman to Integer
 
-ROMAN_VALUE = {
-    "I": 1,
-    "II": 2,
-    "III": 3,
-    "IV": 4,
-    "V": 5,
-    "VI": 6,
-    "VII": 7,
-    "VIII": 8,
-    "IX": 9,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-}
+def romanToInt(s: str) -> int:
+    ROMAN_VALUE = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,}
+    ROMAN_MAP = {"I": 1, "V": 5, "X": 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    n = len(s)
+    num = ROMAN_MAP[s[n - 1]]
+    return num
+    return n
+
+
+print(romanToInt("MCMXCIV"))
