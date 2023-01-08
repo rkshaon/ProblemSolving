ab061e0e7184e6c83ea6447ba2129ab959027747
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-01-09 00:35:10 +0600

    LeetCode # 268 - another solution added

--- 268.py
+++ 268.py
@@ -7,6 +7,9 @@ def missing_number(nums):
 
     return list(set(nums_list_in_range) - set(nums))[0]
 
+    # single line solution
+    return list(set(n for n in range(0, len(nums)+1)) - set(nums))[0]
+
 
 print(missing_number(nums=[3, 0, 1]))
 print(missing_number(nums=[0, 1]))
