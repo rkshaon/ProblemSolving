32fff8c326cf4a5522561231e941ccf5247b3c73
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-13 02:26:02 +0600

    LeetCode # 81 - O(log n) solution

--- 81.py
+++ 81.py
@@ -16,29 +16,70 @@ class Solution:
         # More simple solution 2
         # return True if target in nums else False
 
-        # Algorithmic solution
+        # Algorithmic solution 3
+        # left, right = 0, len(nums) - 1
+
+        # while left <= right:
+        #     if nums[left] == target or nums[right] == target:
+        #         return True
+            
+        #     elif target > nums[left]:
+        #         while left < right and nums[left+1] == nums[left]:
+        #             left += 1
+
+        #         left += 1
+
+        #     elif target < nums[right]:
+        #         while left < right and nums[right-1] == nums[right]:
+        #             right -= 1
+                    
+        #         right -= 1
+            
+        #     else:
+        #         break
+
+        # return False
+        
+        # Algorithm solution 4, O(logn) solution
+        # Algorithm:
+        # 1. Initialize two pointers, 'left' and 'right', where 'left' points to 
+        #   the first element of the array and 'right' points to the last element 
+        #   of the array.
+        # 2. While 'left' is less than or equal to 'right', repeat steps 3 
+        #   through 6:
+        # 3. Compute the middle element of the array as 'mid' = (left + right) / 2.
+        # 4. If the middle element is equal to the target, return 'true'.
+        # 5. If the left half of the array is sorted, i.e., nums[left] <= nums[mid], 
+        #   then check if the target is in the left half, i.e., nums[left] <= target < nums[mid], 
+        #   and adjust the 'right' pointer accordingly.
+        # 6. If the right half of the array is sorted, i.e., nums[mid] <= nums[right], 
+        #   then check if the target is in the right half, i.e., nums[mid] < target <= nums[right], 
+        #   and adjust the 'left' pointer accordingly.
+        # 7. If the target is not found after the while loop, return 'false'.
         left, right = 0, len(nums) - 1
 
         while left <= right:
-            if nums[left] == target or nums[right] == target:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
                 return True
             
-            elif target > nums[left]:
-                while left < right and nums[left+1] == nums[left]:
-                    left += 1
-
+            if nums[left] == nums[mid] and nums[right] == nums[mid]:
                 left += 1
-
-            elif target < nums[right]:
-                while left < right and nums[right-1] == nums[right]:
-                    right -= 1
-                    
                 right -= 1
-            
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid - 1
             else:
-                break
-
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
         return False
+                
     
 
 a = Solution()
