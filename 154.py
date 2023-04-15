93b346111572f12c0d2841e57a51e9f982c372bd
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-15 11:37:05 +0600

    LeetCode # 154 - O(log n) solution

---
+++ 154.py
@@ -0,0 +1,38 @@
+# LeetCode
+# 154. Find Minimum in Rotated Sorted Array II
+# Array, Binary Search
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+from typing import List
+
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return self.search(nums=nums, left=0, right=len(nums)-1)
+
+
+    def search(self, nums: List[int], left: int, right: int) -> int:
+        if left == right:
+            return nums[left]
+        
+        mid = (left + right) // 2
+
+        if nums[mid] > nums[right]:
+            return self.search(nums=nums, left=mid+1, right=right)
+
+        elif nums[mid] < nums[right]:
+            return self.search(nums=nums, left=left, right=mid)
+        
+        else:
+            left_min = self.search(nums=nums, left=left, right=mid)
+            right_min = self.search(nums=nums, left=mid+1, right=right)
+            
+            return min(left_min, right_min)
+
+
+a = Solution()
+
+print(a.findMin(nums = [1,3,5]))
+print(a.findMin(nums = [2,2,2,0,1]))
+print(a.findMin(nums = [1,1]))
