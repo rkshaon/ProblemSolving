6c887133e697276a6ceb5cd1bdf9d8bb4bdb1032
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-08-03 18:36:20 +0600

    LeetCode # 242 - new solution added

--- 242.py
+++ 242.py
@@ -24,13 +24,18 @@
 #         return True
 
 # New solution 2
-from collections import Counter
+# from collections import Counter
  
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return Counter(s) == Counter(t)
     
 
+# New solution 3 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+    
 solution = Solution()
 
 print(solution.isAnagram(s="anagram", t="nagaram"))
