83d3d8f8084cdbc7d09b1a1b9dcd0e883750611e
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-08-03 16:18:03 +0600

    LeetCode # 217 - solution of time complexity O(n), memory complexity O(n)

---
+++ 217.py
@@ -0,0 +1,40 @@
+# 217
+# https://leetcode.com/problems/contains-duplicate/description/
+# Easy
+# Array, Hash Table, Sorting
+
+
+
+from typing import List
+
+
+# Previous accepted solution
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+        
+#         for i in range(0, len(nums)-1):
+#             if nums[i] == nums[i+1]:
+#                 return True
+            
+#         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+
+            hashset.add(n)
+        
+        return False
+
+
+solution = Solution()
+
+print(solution.containsDuplicate(nums=[1, 2, 3, 1]))
+print(solution.containsDuplicate(nums=[1, 2, 3, 4]))
+print(solution.containsDuplicate(nums=[1,1,1,3,3,4,3,2,4,2]))
