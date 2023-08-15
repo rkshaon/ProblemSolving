dab8be5ef9c964d3ccd7ad15e76a7c7a1c07935e
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-08-15 17:46:23 +0600

    LeetCode # 49 - O(m*n) complexity - solution

--- 49.py
+++ 49.py
@@ -4,19 +4,22 @@
 
 
 # My own solution
-def group_anagrams(strs):
-    anagrams = {}
+# def group_anagrams(strs):
+#     anagrams = {}
 
-    for s in strs:
-        sorted_str = ''.join(sorted(s))
+#     for s in strs:
+#         sorted_str = ''.join(sorted(s))
 
-        if sorted_str in anagrams:
-            anagrams[sorted_str].append(s)
-        else:
-            anagrams[sorted_str] = [s]
+#         if sorted_str in anagrams:
+#             anagrams[sorted_str].append(s)
+#         else:
+#             anagrams[sorted_str] = [s]
 
-    return anagrams.values()
+#     return anagrams.values()
 
+# print(group_anagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
+# print(group_anagrams(strs = [""]))
+# print(group_anagrams(strs = ["a"]))
 
 # ChatGPT solution
 # from collections import defaultdict
@@ -36,6 +39,28 @@ def group_anagrams(strs):
 #         # Return the values of the defaultdict
 #         return anagrams.values()
 
-print(group_anagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
-print(group_anagrams(strs = [""]))
-print(group_anagrams(strs = ["a"]))
+# Time Complexity = O(m * n * 26) = O(m * n)
+# Here m = number of the input string
+# Here n = average length of each string
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) # mapping charCount to List of Anagrams
+        
+        for s in strs:
+            count = [0] * 26 # a TO z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            result[tuple(count)].append(s)
+        
+        return result.values()
+
+
+
+print(Solution().groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
+print(Solution().groupAnagrams(strs = [""]))
+print(Solution().groupAnagrams(strs = ["a"]))
