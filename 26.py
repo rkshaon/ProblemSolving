e1881afc8fbc20896185de282fa38a395de1912c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-08-29 19:19:47 +0600

    LeetCode # 26 - linear solution using two pointer

--- 26.py
+++ 26.py
@@ -2,11 +2,26 @@
 # Remove Duplicates from Sorted Array
 # Easy
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# Array, Two Pointer
 
-def remove_duplicates(nums):
-    # nums = set(nums)
-    # return len(nums)
-    return len(set(nums))
 
-print(remove_duplicates([1,1,2]))
-print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]))
+from typing import List
+
+
+# Solution: Two Pointer
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 1
+
+        for right in range(1, len(nums)):
+            if nums[right] != nums[right - 1]:
+                nums[left] = nums[right]
+                left += 1
+            
+        return left, nums
+
+
+
+
+print(Solution().removeDuplicates(nums = [1,1,2]))
+print(Solution().removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))
